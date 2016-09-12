ccd4687cce0f9f13a165a42bf6f16be9032fb0cf
Eric Ma <amcire96@gmail.com>
2016-09-12 11:43:58 -0400

    altered find_ngram_prob so that it filters certain special characters. Currently an issue when user doesn't close () or []... mostly in email section though

--- code.py
+++ code.py
@@ -1,24 +1,112 @@
 import nltk
+import nltk.data
 import numpy as np
+import os
 
-#data_corrected\classification task\atheism\train_docs\atheism_file1.txt
+# data_corrected\classification task\atheism\train_docs
 
 
-def find_ngram_prob(n, tokens):
-    ln = len(tokens)
-    a = {}
-    for i in range(0, ln - n + 1):
-        if n == 1:
-            token_list = tokens[i]
-        else:
-            token_list = ()
-            for j in range(i, i+n):
-                token_list = token_list + (tokens[j],)
-        if token_list in a:
-            a[token_list] += (1./ln)
-        else:
-            a[token_list] = (1./ln)
-    return a
+def find_ngram_counts(dirname):
+    sentence_detector = nltk.data.load("tokenizers/punkt/english.pickle")
+    
+    unigram_counts = {}
+    bigram_counts = {}
+    
+    for filename in os.listdir(dirname):
+        with open(os.path.join(dirname,filename), 'r') as f:
+            text = f.readline()
+            
+            sentence_list = sentence_detector.tokenize(text)
+            # Add sentence boundary tags to each sentence
+            added_sentence_tags_list = ["-s- " + sentence + " -/s-" for sentence in sentence_list]
+#             print(added_sentence_tags_list)
+            
+            # Tokenize the sentences by words
+            tokens = nltk.word_tokenize(" ".join(added_sentence_tags_list))
+#             print(tokens)
+            
+            # Alternative: use counts of '(' ')' and '[' ']' but people forget to close their parentheses/brackets...
+            is_inside_parens = False
+            is_inside_brackets = False
+            
+            prev_word = None
+            
+            for word in tokens:
+                
+                # Design decision to ignore these special characters
+                if (word == "<" or word == ">" or word == "|" or word == "#" or
+                    word == "'" or word == '"'):
+                    continue
+                
+                # Design decision to ignore every thing that is in parentheses/brackets
+                # because this information is by definition superfluous
+                elif word == "(":
+                    is_inside_parens = True
+                    continue
+                
+                elif word == "[":
+                    is_inside_brackets = True
+                    continue
+                
+                elif word == ")":
+                    is_inside_parens = False
+                    continue
+                
+                elif word == "]":
+                    is_inside_brackets = False
+                    continue
+                
+                if is_inside_parens or is_inside_brackets:
+                    continue
+                
+                # Design decision to ignore case
+                word = word.lower()
+               
+                if word not in unigram_counts:
+                    unigram_counts[word] = 1
+                else:
+                    unigram_counts[word] += 1
+                
+                # Cannot compute bigram for first pass
+                if prev_word is not None:
+                    pair = (prev_word, word)
+                    if pair not in bigram_counts:
+                        bigram_counts[pair] = 1
+                    else:
+                        bigram_counts[pair] += 1
+                    
+                prev_word = word
+                
+            if is_inside_parens or is_inside_brackets:
+                print("UNCLOSED PARENS/BRACKETS\n")
+                print(filename)
+    
+#     ln = len(tokens)
+#     a = {}
+#     for i in range(0, ln - n + 1):
+#         if n == 1:
+#             token_list = tokens[i].lower()
+#         else:
+#             token_list = ()
+#             for j in range(i, i+n):
+#                 token_list = token_list + (tokens[j].lower(),)
+#         if token_list in a:
+#             a[token_list] += (1./ln)
+#         else:
+#             a[token_list] = (1./ln)
+    return unigram_counts, bigram_counts
+
+
+def find_ngram_prob(dirname):
+    unigram_counts, bigram_counts = find_ngram_counts(dirname)
+    
+    unigram_probs = {k: v/sum(unigram_counts.values()) for k, v in unigram_counts.items()}
+    
+    # Bigram probabilities = count(w(n-1) w(n)) / count(w(n-1))
+    bigram_probs = {k: v/unigram_counts[k[0]] for k, v in bigram_counts.items()}
+    
+    return unigram_probs, bigram_probs
+
 
 #remove used words? how to deal with multiple punctuation in a row?
 def rand_sentence(prob_table, n):
@@ -49,18 +137,25 @@ def rand_sentence(prob_table, n):
             sentence += " "+match
     return sentence
 
+
+def main():
+    n = int(input("Enter value of n (only 1 or 2)\n"))
+#     text = ""
+    
+    data = input("Enter file or directory name of corpus: \n")
+    
+    unigram_prob, bigram_prob = find_ngram_prob(data)
+    
+    if n == 1:
+        prob_table = unigram_prob
+    elif n == 2:
+        prob_table = bigram_prob
+    else:
+        print("Can only do unigram and bigram\n")
+    
+#     print(prob_table)
+    print(rand_sentence(prob_table, n))
+
+
 if __name__ == '__main__':
-    n = int(input("Enter value of n \n"))
-    text = ""
-    while True:
-        data = input("Enter file, or enter blank if no more files: \n")
-        if not data.lower() == "":
-            with open(data, 'r') as my_file:
-                text += my_file.read().replace('\n', '')
-            continue
-        else:
-            break
-    token = nltk.word_tokenize(text)
-    ng = find_ngram_prob(n, token)
-    #print ng
-    print(rand_sentence(ng, n))
+    main()
